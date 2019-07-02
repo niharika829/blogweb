27dce2c4b11b8332844d0580de71b55ded029240
niharika829 <49246107+niharika829@users.noreply.github.com>
2019-07-02 23:35:10 +0530

    added JS functionality to create post button(part 2)

--- stub/js/index.js
+++ stub/js/index.js
@@ -3,3 +3,15 @@ var modal_create = document.getElementById("myModalcreate");
 
 // Get the button that opens the modal
 var btn_create = document.getElementById("myBtn_create");
+// Get the <span> element that closes the modal
+var span_create = document.getElementsByClassName("close_create")[0];
+
+// When the user clicks the button, open the modal 
+btn_create.onclick = function() {
+  modal_create.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span_create.onclick = function() {
+  modal_create.style.display = "none";
+}
