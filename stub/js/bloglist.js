fb18135c1816dcfbc013129dbc666724ef26edb3
niharika829 <49246107+niharika829@users.noreply.github.com>
2019-07-04 19:57:06 +0530

    added reference to the trash button(getElementById)

---
+++ stub/js/bloglist.js
@@ -0,0 +1,6 @@
+// Get the button that opens the modal
+var btn_trash1 = document.getElementById("fa1");
+var btn_trash2 = document.getElementById("fa2");
+var btn_trash3 = document.getElementById("fa3");
+var btn_trash4 = document.getElementById("fa4");
+var btn_trash5 = document.getElementById("fa5");
