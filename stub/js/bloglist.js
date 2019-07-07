402518d4f49c946d1bd7c33bc2a227ee06b9c357
niharika829 <49246107+niharika829@users.noreply.github.com>
2019-07-07 14:57:36 +0530

    added comments in bloglist.js

--- stub/js/bloglist.js
+++ stub/js/bloglist.js
@@ -4,27 +4,36 @@ var btn_trash2 = document.getElementById("fa2");
 var btn_trash3 = document.getElementById("fa3");
 var btn_trash4 = document.getElementById("fa4");
 var btn_trash5 = document.getElementById("fa5");
+
+
+
 // Get the modal
 var modal_trash = document.getElementById("myModaltrash");
 
-btn_trash1.onclick = function() {
-  modal_trash.style.display = "block";
-}
-btn_trash2.onclick = function() {
-  modal_trash.style.display = "block";
-}
-btn_trash3.onclick = function() {
-  modal_trash.style.display = "block";
-}
-btn_trash4.onclick = function() {
-  modal_trash.style.display = "block";
-}
-btn_trash5.onclick = function() {
-  modal_trash.style.display = "block";
-}
 
+
+//by clicking the button the modal should appear
+btn_trash1.onclick = function() {
+										modal_trash.style.display = "block";
+									}
+btn_trash2.onclick = function() {
+										modal_trash.style.display = "block";
+									}
+btn_trash3.onclick = function() {
+										modal_trash.style.display = "block";
+									}
+btn_trash4.onclick = function() {
+										modal_trash.style.display = "block";
+									}
+btn_trash5.onclick = function() {
+										modal_trash.style.display = "block";
+									}
+									
+									
+									
+//by clicking the no button the modal will disappear
 var btn_no = document.getElementById("no");
 btn_no.onclick = function() {
-  modal_trash.style.display = "none";
-}
+										modal_trash.style.display = "none";
+									}
  
